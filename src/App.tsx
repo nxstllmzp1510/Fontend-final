2ccafa555bc81f944edc430589dffb47c2a853c9
AchirayaSE67 <achiraya.kanya@gmail.com>
2026-08-10 13:43:32 +0700

    create  login and signup pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import LoginPage from "./pages/LoginPage";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
 
 function App() {
-  return <LoginPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* ถ้าเปิดหน้าแรก (/) ให้แสดงหน้า Login */}
+        <Route path="/" element={<LoginPage />} />
+        
+        {/* ถ้าเปิด /signup ให้แสดงหน้า Signup */}
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
